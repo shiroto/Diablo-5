e4c75c6ff25f5b901ce2d8fc160b6885c5e9edb1
Julius Dockwarder <hello@julius.dev>
2021-06-24 17:20:43 +0200

    feat: implements jump and dash

--- Assets/_GAME/Scripts/ActorBehaviours/PlayerBehaviour.cs
+++ Assets/_GAME/Scripts/ActorBehaviours/PlayerBehaviour.cs
@@ -8,6 +8,15 @@ public class PlayerBehaviour : CharacterBehaviour
     [SerializeField]
     private NavMeshAgent agent;
 
+    public Vector3 jump;
+    public float jumpForce = 4.0f;
+    
+    public bool isGrounded;
+    public bool isDashing;
+
+    Rigidbody rb;
+    NavMeshAgent nma;
+
     private void AddAction(ActionHandler ah)
     {
         actionManager.AddAction(ah);
@@ -15,6 +24,11 @@ public class PlayerBehaviour : CharacterBehaviour
 
     private void FixedUpdate()
     {
+    }
+
+    void OnCollisionStay() {
+        enableNavigator();
+        isGrounded = true;
     }
 
     // Start is called before the first frame update
@@ -27,6 +41,11 @@ public class PlayerBehaviour : CharacterBehaviour
         cdr = 0.3f;
         range = 2;
 
+        rb = GetComponent<Rigidbody>();
+        nma = GetComponent<NavMeshAgent>();
+
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+
         //
         actionManager = gameObject.AddComponent<ActionManager>() as ActionManager;
         actionManager.SetUser(gameObject);
@@ -37,36 +56,76 @@ public class PlayerBehaviour : CharacterBehaviour
         //
     }
 
+    private void watchNavAgent() {
+        if(agent.enabled) {
+            agent.speed = baseMovementSpeed * movementSpeedModifier;
+
+            if (actionManager.IsCasting())
+            {
+                agent.isStopped = true;
+            }
+            else
+            {
+                agent.isStopped = false;
+            }
+        }
+    }
+
+    private void disableNavigator() {
+        rb.isKinematic = false;
+        nma.enabled = false;
+    }
+
+    private void enableNavigator() {
+        rb.isKinematic = true;
+        nma.enabled = true;
+    }
+
+    private void performDash() {
+        Vector3 movementVelocity = nma.velocity;
+        movementVelocity.y = 0;
+
+        disableNavigator();
+
+        rb.AddForce(movementVelocity * 10.0f, ForceMode.Impulse);
+    }
+
+    private void watchDashVelocity() {
+        if(isDashing && rb.velocity == Vector3.negativeInfinity) {
+            enableNavigator();
+        }
+    }
+
+    private void watchJumpAction() {
+        if(Input.GetKeyDown(KeyCode.Space) && !isDashing) {
+            Vector3 movementVelocity = nma.velocity;
+
+            disableNavigator();
+
+            isGrounded = false;
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            rb.AddForce(movementVelocity * 50, ForceMode.Impulse);
+        }
+    }
+
     // Update is called once per frame
     private void Update()
     {
-        //set agents speed every frame
-        agent.speed = baseMovementSpeed * movementSpeedModifier;
+        watchNavAgent();
+        watchJumpAction();
 
-        if (actionManager.IsCasting())
-        {
-            agent.isStopped = true;
-        }
-        else
-        {
-            agent.isStopped = false;
-        }
-
-        //handle input
         if (Stuff.GetButtonDown("Hotkey1"))
         {
             Vector3 temp = Stuff.Intersect();
-            if (!temp.Equals(Vector3.negativeInfinity))
+            if (nma.enabled && !temp.Equals(Vector3.negativeInfinity))
             {
                 agent.SetDestination(temp);
             }
         }
         if (Stuff.GetButtonDown("Hotkey2"))
         {
-            if (actionManager.Use("Action1"))
-            {
-                //action was used
-            }
+            performDash();
         }
         if (Stuff.GetButtonDown("Hotkey3"))
         {
