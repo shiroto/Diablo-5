6b887844cfe0abae292b25bbc7c72cffd2de2c1c
Lennard Scheibel <contact@lennardscheibel.de>
2021-07-13 16:03:52 +0200

    Run test

--- Assets/_GAME/Scripts/Stuff.cs
+++ Assets/_GAME/Scripts/Stuff.cs
@@ -6,7 +6,6 @@ public class Stuff : MonoBehaviour
 {
 
     static Dictionary<string, KeyCode> buttons;
-    public static Plane world;
 
     void Start()
     {
@@ -28,31 +27,5 @@ public class Stuff : MonoBehaviour
             return false;
         }
         return Input.GetKey(buttons[buttonName]);
-    }
-
-    public static Vector3 Intersect()
-    {
-
-        // Bit shift the index of the layer (8) to get a bit mask
-        int layerMask = 1 << 8;
-
-        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(r.origin, r.direction, out RaycastHit hit, Mathf.Infinity, layerMask))
-        {
-            var h = r.GetPoint(hit.distance);
-            return h;
-        }
-        else
-        {
-            return Vector3.negativeInfinity;
-        }
-    }
-
-    public static bool ApproxEqual(Vector3 v1, Vector3 v2, float bound = 0.5f)
-    {
-        if ((v1.x - v2.x) * (v1.x - v2.x) + (v1.z - v2.z) * (v1.z - v2.z) <= bound)
-            return true;
-        return false;
     }
 }
